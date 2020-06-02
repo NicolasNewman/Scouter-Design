2d388873a007ef72a622b04835a82fd1e0ed8ae3
Nicolas Newman <nanewman01@gmail.com>
2020-06-02 13:06:07 -0500

    Initial commit

---
+++ app/components/Home.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { Component } from 'react';
+import { RouteComponentProps } from 'react-router';
+import { Redirect } from 'react-router';
+import DataStore from 'app/classes/DataStore';
+
+interface IProps extends RouteComponentProps<any> {
+    dataStore: DataStore;
+}
+
+export default class Home extends Component<IProps> {
+    props: IProps;
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        // if (this.state.toHome) {
+        //     return <Redirect to="/home" />;
+        // }
+        return (
+            <div>
+                <h2>Home</h2>
+            </div>
+        );
+    }
+}
---
+++ app/containers/HomePage.tsx
@@ -0,0 +1,19 @@
+import { bindActionCreators, Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import Home from '../components/Home';
+import CounterActions from '../actions/counter';
+
+function mapStateToProps(state, ownProps) {
+    return {
+        dataStore: ownProps.dataStore
+    };
+}
+
+function mapDispatchToProps(dispatch: Dispatch) {
+    return bindActionCreators(CounterActions, dispatch);
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Home);
