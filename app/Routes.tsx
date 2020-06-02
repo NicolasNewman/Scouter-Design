21af7258d5ea619b0b4f758a1d54e3ce568be499
NicolasNewman <nnewman316@gmail.com>
2020-06-02 14:01:33 -0500

    updated the router to handle the two pages

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -2,13 +2,16 @@ import * as React from 'react';
 import { Component } from 'react';
 import { Switch, Route } from 'react-router';
 const routes = require('./constants/routes.json');
+import sizes from './constants/sizes';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
-// import CounterPage from './containers/CounterPage';
+import DesignerPage from './containers/DesignerPage';
 import DataStore from './classes/DataStore';
+import IpcInterface from './classes/IpcInterface';
 
 export default class Routes extends Component {
     private dataStore: DataStore = new DataStore();
+    private ipcInterface: IpcInterface = new IpcInterface();
 
     render() {
         return (
@@ -16,9 +19,23 @@ export default class Routes extends Component {
                 <Switch>
                     <Route
                         path={routes.HOME}
-                        component={() => (
-                            <HomePage dataStore={this.dataStore} />
-                        )}
+                        component={() => {
+                            this.ipcInterface.resizeWindow(
+                                sizes.homeWindow.width,
+                                sizes.homeWindow.height
+                            );
+                            return <HomePage dataStore={this.dataStore} />;
+                        }}
+                    />
+                    <Route
+                        path={routes.DESIGNER}
+                        component={() => {
+                            this.ipcInterface.resizeWindow(
+                                sizes.designerWindow.width,
+                                sizes.designerWindow.height
+                            );
+                            <DesignerPage dataStore={this.dataStore} />;
+                        }}
                     />
                 </Switch>
             </App>
