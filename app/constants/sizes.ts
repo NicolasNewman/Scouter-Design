b29c2a5cd23132699da94dbc9011bb4216ee0219
NicolasNewman <nnewman316@gmail.com>
2020-06-02 14:00:16 -0500

    added ipc interface

---
+++ app/constants/sizes.ts
@@ -0,0 +1,12 @@
+const sizes = {
+    homeWindow: {
+        width: 350,
+        height: 500
+    },
+    designerWindow: {
+        width: 1000,
+        height: 600
+    }
+};
+
+export default sizes;
