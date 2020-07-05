d5a40c04de3ba4794295d55546866320e1dbd122
NicolasNewman <nnewman316@gmail.com>
2020-07-04 19:28:33 -0500

    added a util function to camalize a string

--- app/utils/helper.ts
+++ app/utils/helper.ts
@@ -17,3 +17,18 @@ export const formatStateName = (name: string): string => {
     let formated = name.replace(/ /g, '_').toUpperCase();
     return formated.substr(0, 5) === 'STATE' ? formated : `STATE_${formated}`;
 };
+
+/**
+ * Converts the passed string from any casing into camel case
+ * eg: "Goal Shot Events" -> "goalShotEvents"
+ *
+ * https://stackoverflow.com/questions/2970525
+ *
+ * @param str - the string to convert to camel case
+ */
+export const toCamelCase = str => {
+    return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, (match, index) => {
+        if (+match === 0) return ''; // or if (/\s+/.test(match)) for white spaces
+        return index === 0 ? match.toLowerCase() : match.toUpperCase();
+    });
+};
