406b64369a2dc8698a3cfb4d770ca83a30d50127
NicolasNewman <nnewman316@gmail.com>
2020-07-05 09:09:52 -0500

    began implementing group customization options

--- app/classes/models/FormGroup.ts
+++ app/classes/models/FormGroup.ts
@@ -1,8 +1,8 @@
 import FormButton from './FormButton';
 
 type GroupOptions = {
-    cols?: string;
-    rows?: string;
+    colCount?: number;
+    rowCount?: number;
     templareArea?: string;
     gridAreaName?: string;
     name: string;
@@ -14,8 +14,10 @@ type GroupOptions = {
 export default class FormGroup {
     // how many columns of buttons are in the group
     private cols: string;
+    private colCount: number;
     // how many rows of buttons are in the group
     private rows: string;
+    private rowCount: number;
     // the order to arrange each button within the group
     private templateArea: string;
     // the identifier used by the parent grid for placement
@@ -26,24 +28,96 @@ export default class FormGroup {
     private formButtons: Array<FormButton>;
 
     constructor(options: GroupOptions) {
-        this.cols = options.cols;
-        this.rows = options.rows;
+        this.colCount = options.colCount ? options.colCount : 2;
+        this.updateCols();
+        this.rowCount = options.rowCount ? options.rowCount : 3;
+        this.updateRows();
         this.templateArea = options.templareArea;
         this.gridAreaName = options.gridAreaName;
         this.name = options.name;
         this.formButtons = [];
     }
 
-    setName = (name: string) => (this.name = name);
-    setCols = (cols: string) => (this.cols = cols);
-    setRows = (rows: string) => (this.rows = rows);
+    private updateCols = () => {
+        let cols = '';
+        for (let i = 0; i < this.colCount; i++) {
+            if (i < this.colCount - 1) {
+                cols += '1fr ';
+            } else {
+                cols += '1fr';
+            }
+        }
+        this.cols = cols;
+    };
+
+    private updateRows = () => {
+        let rows = '10% ';
+        let percentage = `${90.0 / (this.rowCount - 1)}%`;
+        for (let i = 1; i < this.rowCount; i++) {
+            if (i < this.rowCount - 1) {
+                rows += percentage + ' ';
+            } else {
+                rows += percentage;
+            }
+        }
+        this.rows = rows;
+    };
+
+    toString = () => {
+        return `
+        name: ${this.name}
+        gridAreaName: ${this.gridAreaName}
+        dimensions: ${this.rowCount}x${this.colCount}
+        row template: "${this.rows}"
+        col template: "${this.cols}"`;
+    };
+
+    setCols = (cols: string) => {
+        this.cols = cols;
+        this.colCount = this.cols.replace(/[^ ]/g, '').length + 1;
+    };
+    setColCount = (colCount: number) => {
+        this.colCount = colCount;
+        let cols = '';
+        for (let i = 0; i < colCount; i++) {
+            if (i < colCount - 1) {
+                cols += '1fr ';
+            } else {
+                cols += '1fr';
+            }
+        }
+        this.cols = cols;
+    };
+
+    setRows = (rows: string) => {
+        this.rows = rows;
+        this.rowCount = this.rows.replace(/[^ ]/g, '').length + 1;
+    };
+    setRowCount = (rowCount: number) => {
+        this.rowCount = rowCount;
+        let rows = '10% ';
+        let percentage = `${90.0 / (rowCount - 1)}%`;
+        for (let i = 1; i < rowCount; i++) {
+            if (i < rowCount - 1) {
+                rows += percentage + ' ';
+            } else {
+                rows += percentage;
+            }
+        }
+        this.rows = rows;
+    };
+
     setTemplateArea = (templateArea: string) =>
         (this.templateArea = templateArea);
+    setName = (name: string) => (this.name = name);
     setGridAreaName = (gridAreaName: string) =>
         (this.gridAreaName = gridAreaName);
 
     getName = () => this.name;
     getGridAreaName = () => this.gridAreaName;
+
+    getRowCount = () => this.rowCount;
+    getColCount = () => this.colCount;
 
     addButton(button: FormButton) {
         this.formButtons.push(button);
--- app/components/Designer/GroupCreator.tsx
+++ app/components/Designer/GroupCreator.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { Component } from 'react';
 import { EventDataArray, StateDataArray } from 'app/types/types';
-import { Button, Select, Modal, Input } from 'antd';
+import { Button, Select, Modal, Input, InputNumber } from 'antd';
 import FormGroup from '../../classes/models/FormGroup';
 import { toCamelCase } from '../../utils/helper';
+import Grid from '../Grid';
 
 interface IProps {
     // redux - event
@@ -22,18 +23,24 @@ interface IProps {
 
 interface IState {
     modalVisible: boolean;
+    targetGroup: FormGroup;
 }
 
 export default class GroupCreator extends Component<IProps, IState> {
     props: IProps;
     input: React.RefObject<Input>;
+    rowRef: React.RefObject<any>;
+    colRef: React.RefObject<any>;
 
     constructor(props) {
         super(props);
         this.state = {
-            modalVisible: false
+            modalVisible: false,
+            targetGroup: null
         };
         this.input = React.createRef();
+        this.rowRef = React.createRef();
+        this.colRef = React.createRef();
     }
 
     addGroup = () => {
@@ -58,7 +65,7 @@ export default class GroupCreator extends Component<IProps, IState> {
                     <Input placeholder="Group name" ref={this.input}></Input>
                 </Modal>
                 <div className="group-creator__groups">
-                    <div className="">
+                    <div className="group-creator__groups--manage">
                         <Button
                             className="mr-1"
                             type="primary"
@@ -73,6 +80,25 @@ export default class GroupCreator extends Component<IProps, IState> {
                         </Button>
                         <Select
                             className="w-7"
+                            onChange={val => {
+                                const targetGroup = this.props.groups.find(
+                                    group => group.getName() === val
+                                );
+                                this.setState({
+                                    targetGroup
+                                });
+
+                                const rowCount = targetGroup.getRowCount();
+                                const colCount = targetGroup.getColCount();
+                                this.rowRef.current.setState({
+                                    inputValue: `${rowCount}`,
+                                    value: rowCount
+                                });
+                                this.colRef.current.setState({
+                                    inputValue: `${colCount}`,
+                                    value: colCount
+                                });
+                            }}
                             options={this.props.groups.map(group => {
                                 return {
                                     key: group.getGridAreaName(),
@@ -81,9 +107,74 @@ export default class GroupCreator extends Component<IProps, IState> {
                             })}
                         />
                     </div>
-                    <div className=""></div>
+                    <div
+                        className="group-creator__groups--modify mt-2"
+                        hidden={this.state.targetGroup === null}
+                    >
+                        <div>
+                            <p>
+                                {this.state.targetGroup !== null
+                                    ? this.state.targetGroup.getName()
+                                    : ''}
+                            </p>
+                        </div>
+                        <div>
+                            <span>Rows: </span>
+                            <InputNumber
+                                ref={this.rowRef}
+                                min={2}
+                                // defaultValue={this.state.targetRows}
+                                className="w-3 mr-1"
+                            />
+                            <span>Cols: </span>
+                            <InputNumber
+                                ref={this.colRef}
+                                min={1}
+                                // defaultValue={this.state.targetCols}
+                                className="w-3"
+                            />
+                        </div>
+                        <div className="mt-1">
+                            <Button
+                                onClick={e => {
+                                    const rows = this.rowRef.current.state
+                                        .value;
+                                    const cols = this.colRef.current.state
+                                        .value;
+                                    this.state.targetGroup.setRowCount(rows);
+                                    this.state.targetGroup.setColCount(cols);
+                                    console.log(
+                                        this.state.targetGroup.toString()
+                                    );
+                                    console.log(this.input);
+                                    console.log(this.colRef);
+                                }}
+                                type="primary"
+                            >
+                                Update
+                            </Button>
+                        </div>
+                    </div>
                 </div>
-                <div className="group-creator__builder">B</div>
+                <div className="group-creator__builder">
+                    <Grid
+                        gridAreaName="state"
+                        className="input-grid__child"
+                        cols="1fr 1fr"
+                        rows="10% 30% 30% 30%"
+                        templateArea="
+                            'title      title   '
+                            'gather     shooting'
+                            'wheel      climbing'
+                            'defending  defended'"
+                        gridElements={[
+                            <div className="input-grid__title">
+                                <p>States</p>
+                            </div>
+                        ]}
+                    />
+                    ,
+                </div>
                 <div className="group-creator__editor">C</div>
             </div>
         );
